310a5e24a9d8a47488e8e9489ad4e24397e9788d
Jeremy Ong <jeremy@micepad.co>
2016-07-29 23:12:13 +0800

    - data type confirmed

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -15,39 +15,76 @@
 var serverUrl = 'http://128.199.195.84:3000/';
 // var serverUrl = 'http://localhost:80/';
 
+yArr = [
+    [[10, 'q'], [20, 'w'], [30, 'e'], [40, 'r'], [50, 't'], [60, 'y'], [70, 'u'], [80, 'i'], [90, 'o'], [100, 'p']],
+    [[15, 'a'], [25, 's'], [35, 'd'], [45, 'f'], [55, 'g'], [65, 'h'], [75, 'j'], [85, 'k'], [100, 'l']],
+    [[13.8, 'shift'], [25.5, 'z'], [35.5, 'x'], [45.5, 'c'], [55.5, 'v'], [65.5, 'b'], [75.5, 'n'], [85.5, 'm'], [100, 'bkspace']],
+    [[12.5, '123'], [25.5, 'globe'], [35.5, 'mic'], [75.5, 'space'], [100, 'return']]
+];
+
+
 var ctrl = {
     tempResult: [],
+    temp: {},
+    input: '',
+    shift: false,
     init: function () {
 
         $('#keyboardContainer').css({top: keyboardMarginTop});
 
 
         $('#keyboardContainer').on('touchstart', function (event) {
+            ctrl.prepare(event);
+        });
+        $('#keyboardContainer').on('touchend', function (event) {
             ctrl.record(event);
         });
+    },
+    prepare: function (e) {
+        var oe = e.originalEvent;
+        console.log(oe);
+
+        var x = oe.changedTouches[0].clientX / keyboardWidth * 100;
+        var y = (oe.changedTouches[0].clientY - keyboardMarginTop) / keyboardHeight * 100;
+        // console.log(x / keyboardWidth * 100, y / keyboardHeight * 100);
+
+        var letter = '';
+        xArr = [];
+        if (y <= 25) {
+            xArr = yArr[0]
+        } else if (y <= 50) {
+            xArr = yArr[1]
+        } else if (y <= 75) {
+            xArr = yArr[2]
+        } else if (y <= 100) {
+            xArr = yArr[3]
+        }
+
+        for (var i = 0; i < xArr.length; i++) {
+            var xCoord = xArr[i][0];
+            if (x < xCoord) {
+                letter = xArr[i][1];
+                break;
+            }
+        }
+        var downResult = {
+            x: x,
+            y: y,
+            time: oe.timeStamp,
+        };
+
+        ctrl.temp[letter] = downResult;
     },
     record: function (e) {
         // console.log(e);
         var oe = e.originalEvent;
         console.log(oe);
 
-        var x = oe.changedTouches[0].clientX;
-        var y = oe.changedTouches[0].clientY - keyboardMarginTop;
+        var x = oe.changedTouches[0].clientX / keyboardWidth * 100;
+        var y = (oe.changedTouches[0].clientY - keyboardMarginTop) / keyboardHeight * 100;
         // console.log(x / keyboardWidth * 100, y / keyboardHeight * 100);
 
-        x = x / keyboardWidth * 100
-        y = y / keyboardHeight * 100
         var letter = '';
-
-
-        //TODO: FILL UP LETTER!
-        yArr = [
-            [[10, 'q'], [20, 'w'], [30, 'e'], [40, 'r'], [50, 't'], [60, 'y'], [70, 'u'], [80, 'i'], [90, 'o'], [100, 'p']],
-            [[15, 'a'], [25, 's'], [35, 'd'], [45, 'f'], [55, 'g'], [65, 'h'], [75, 'j'], [85, 'k'], [100, 'l']],
-            [[13.8, 'shift'], [25.5, 'z'], [35.5, 'x'], [45.5, 'c'], [55.5, 'v'], [65.5, 'b'], [75.5, 'n'], [85.5, 'm'], [100, 'bkspace']],
-            [[12.5, '123'], [25.5, 'globe'], [35.5, 'mic'], [75.5, 'space'], [100, 'return']]
-        ];
-
         xArr = [];
         if (y <= 25) {
             xArr = yArr[0]
@@ -68,17 +105,53 @@ var ctrl = {
         }
 
 
-        console.log(letter);
-        var result = {
+        if (letter != 'bkspace' && letter != 'shift' && letter != 'globe' && letter != 'mic' && letter != 'return' && letter != 'space') {
+            ctrl.input += letter;
+            $('#replacehere').val(ctrl.input);
+
+        } else if (letter == 'space') {
+            ctrl.input += ' ';
+            $('#replacehere').val(ctrl.input);
+        } else if (letter == 'bkspace') {
+            ctrl.input = ctrl.input.substring(0, ctrl.input.length - 1);
+            $('#replacehere').val(ctrl.input);
+        }
+
+
+        var downResult = _.clone(ctrl.temp[letter]);
+        ctrl.temp[letter] = null;
+
+        if (!downResult){
+            return;
+        }
+
+
+        var upResult = {
             x: x,
             y: y,
             time: oe.timeStamp,
-            letter: letter
         }
+
+        var result = {
+            up: upResult,
+            down: downResult,
+            letter: letter
+        };
+
         ctrl.tempResult.push(result);
+
+
         if (result.letter == 'return') {
-            console.log('result', ctrl.tempResult);
-            $('#resulthere').val(JSON.stringify(ctrl.tempResult));
+
+            var finalResult = {
+                data: ctrl.tempResult,
+                id: '1',
+                imposter: 'false',
+                text: ctrl.input
+            }
+
+            console.log('result', finalResult);
+            $('#resulthere').val(JSON.stringify(finalResult));
 
             var csrftoken = '';
             var cookies = document.cookie.split(';');
@@ -96,12 +169,12 @@ var ctrl = {
                     xhr.setRequestHeader('X-CSRFToken', csrftoken);
                 },
                 type: 'POST',
-                data: JSON.stringify(ctrl.tempResult),
+                data: JSON.stringify(finalResult),
                 dataType: 'json',
-                success: function(res){
+                success: function (res) {
                     console.log(res);
                 },
-                error: function(err){
+                error: function (err) {
                     console.log(err);
                 }
             });
